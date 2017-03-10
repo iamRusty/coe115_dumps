f637ee2812dded366a66e9892cbb11c2f6ac5ef9
Rusty <rustymina@gmail.com>
2017-03-11 01:05:53 +0800

    Rewrote LCD

--- lcd.s
+++ lcd.s
@@ -1,183 +1,252 @@
-.include "p24FJ64GB002.inc"
-.global __reset
+; ******************************************************************************
+; LCD.s - Interface an LCD with PIC24F Microchip
+; 
+; MIT License
+;
+; Copyright (c) 2017 Rusty Mina (rusty.mina@eee.upd.edu.ph)
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
+;    
+; ******************************************************************************
     
+; ***************************** Reference **************************************
+;    
+; 1. LCD Dot Matrix Manual (Page 28 to 46)
+; http://www-inst.eecs.berkeley.edu/~cs150/fa01/labs/project/LCD_User_Manual.pdf
+;    
+;*******************************************************************************    
+    
+.include "p24FJ64GA002.inc"
+.global __reset
+
 .bss
-    i: .space 2
-    j: .space 2
+    i: .space 2    
+    
+config __CONFIG1, FWDTEN_OFF & JTAGEN_OFF
+config __CONFIG2, POSCMOD_NONE & OSCIOFNC_OFF & FCKSM_CSDCMD & FNOSC_FRCPLL   
 
 .text
 __reset:
+; *************************** Initialize Stack Pointer *************************
+MOV	    #__SP_init, W15
+MOV	    #__SPLIM_init, W0
+MOV	    W0, SPLIM
     
-mov #__SP_init, W15
-mov #__SPLIM_init, W0
-mov W0, SPLIM
-
-mov #0x0000, W0
-mov W0, TRISB      /* B - Output */
-mov #0xFFFF, W0
-mov W0, TRISA      /* A - Input  */     
+; ***************************** Initialize GPIO ********************************
+mov	    #0x0001, W0
+mov	    W0, TRISB
+mov	    #0xFFFF, W0
+mov	    W0, AD1PCFG
     
-/* Main */
-CALL    delay15ms
-
-MOV	#0x03, W0
-CALL	send4ToLCD
-CALL	delay4_1ms
+; ************************** Initialize LCD (4-bit) ****************************
+init:    
+; No data should be transferred to or from the display for 15ms    
+    CALL    delay15ms
+; Function Set Command: (8-Bit interface)   
+    MOV	    #0x0003, W0		
+    CALL    send4ToLCD
+    CALL    delay4_1ms
     
-CALL	send4ToLCD
-CALL	delay100us
+    CALL    send4ToLCD
+    CALL    delay100us
+
+; After this command is written, BF can be checked    
+    CALL    send4ToLCD
+    CALL    delay15ms
+
+; Function Set: Sets interface to 4-bit    
+    MOV	    #0x0002, W0		
+    CALL    send8ToLCD
+    CALL    delay15ms
+
+; Function Set (Interface = 4-bit, 
+;   Set N for number of lines	(1)
+;   Set F for character font    (0)
+    MOV	    #0x0028, W0		
+    CALL    send8ToLCD		
+    call    delay15ms		
+
+; Display OFF    
+    MOV	    #0x0008, W0		
+    CALL    send8ToLCD
+    CALL    delay15ms
+
+; Clear Display    
+    MOV	    #0x0001, W0		
+    CALL    send8ToLCD
+    CALL    delay15ms
+
+; Entry Mode Set   
+    MOV	    #0x0006, W0		
+    CALL    send8ToLCD
+    call    delay15ms
+
+; Display ON
+;   Set C for Cursor	(1)
+;   Set B for Blink	(1)
+    MOV	    #0x000F, W0		
+    CALL    send8ToLCD
+    CALL    delay15ms    
+; ==============================================================================
     
-CALL	send4ToLCD
+; ************************** Main Program Loop *********************************
+fl:
+    call clear_display
+    call first_name
+    call switch_line
+    call last_name
+    poll1:
+	mov PORTB, W7  
+	btsc W7, #0	    ; skip next if PORTB<0> is 0
+	goto poll1
+	goto lf
+	
+lf:
+    call clear_display
+    call last_name
+    call switch_line
+    call first_name
+    poll2:
+	mov PORTB, W7
+	btss W7, #0	    ; skip next if PORTB<0> is 1
+	goto poll2
+	goto fl       
+; ==============================================================================
 
-MOV	#0x02, W0
-CALL	send4ToLCD
-
-MOV	#0x28, W0
-CALL	send8ToLCD
-    
-MOV	#0x08, W0
-CALL	send8ToLCD
-    
-MOV	#0x01, W0
-CALL	send8ToLCD
-
-MOV	#0x06, W0
-CALL	send8ToLCD
-    
-MOV	#0x0F, W0
-CALL	send8ToLCD
-/*----------------------------*/
-/* Initialization Complete    */
-/*----------------------------*/
-
-initial_display:
-    mov  #0x80, W0
-    call send8ToLCD
-    call surname
-    mov  #0xC0, W0
-    call send8ToLCD
-    mov #0x1, W9
-
-polling:
-    mov PORTB, W7  
-    btsc.b W7, #0  ; skip next if PORTB<0> is 0
-    goto polling
-    goto switch_lines
-
-switch_lines:
-    btsc.b W9, #0  ; skip next if W9<0> is 0
-    goto original
-    goto fake
-
-original:
-    mov  #0x80, W0
-    call send8ToLCD
-    call surname
-    mov  #0xC0, W0
-    call send8ToLCD
-    mov #0x1, W9
-    goto polling
-
-fake:    
-    mov  #0x80, W0
-    call send8ToLCD
-    call surname
-    mov  #0xC0, W0
-    call send8ToLCD
-    mov #0x0, W9
-    goto polling
-
-surname:
+; ********************************** Functions *********************************	
+last_name:
     mov #0x4D, W0     ; M
-    call send8ToLCD
-
+    call send8ToLCD_data
+    call delay15ms
+    
     mov #0x69, W0     ; i
-    call send8ToLCD
+    call send8ToLCD_data
+    call delay15ms
     
     mov #0x6E, W0     ; n
-    call send8ToLCD
-
+    call send8ToLCD_data
+    call delay15ms
+    
     mov #0x61, W0     ; a
-    call send8ToLCD
-
-    return
-
-given_name:
+    call send8ToLCD_data
+    call delay15ms 
+    return    
+    
+first_name:
     mov #0x52, W0     ; R
-    call send8ToLCD
-
+    call send8ToLCD_data
+    call delay15ms
+    
     mov #0x75, W0     ; u
-    call send8ToLCD
-
+    call send8ToLCD_data
+    call delay15ms
+    
     mov #0x73, W0     ; s
-    call send8ToLCD
-
+    call send8ToLCD_data
+    call delay15ms
+    
     mov #0x74, W0     ; t
-    call send8ToLCD
-
+    call send8ToLCD_data
+    call delay15ms
+    
     mov #0x79, W0     ; y
-    call send8ToLCD
-
-    return
-    
-    
-/* Functions */
-send4ToLCD:
-    /* Extract RS Bit and store in PORTB<13> */
-    btsc.b W0, #4		; Skip next instruction if W0<4> is 0
-    goto RS_1_LCD_4 
-    bclr PORTB, #13
-    goto done_with_RS_LCD_4
-    RS_1_LCD_4:
-    bset PORTB, #13
-    done_with_RS_LCD_4:
-    bset PORTB, #14	; Set E bit
-    /* Sketchy Code */
-    MOV PORTB, W3	
-    MOV #0xF0FF, W4
-    AND W4, W3, W3	; Preserve value of PORTB except for PORTB[11:8]
-    MOV #0x000F, W5
-    AND W5, W0, W5	; Extract W0[3:0] bits
-    SL	W5, #8, W5	; Shift W0[3:0] -> W0[11:8]
-    IOR W5, W3, W3
-    MOV W3, PORTB
-    bclr PORT, #14
-    return
-    
-send8ToLCD:
-    /* Extract RS Bit and store in PORTB<13> */
-    btsc.b W0, #8		; Skip next instruction if W0<8> is 0
-    goto RS_1_LCD_8 
-    bclr PORTB, #13
-    goto done_with_RS_LCD_8
-    RS_1_LCD_8:
-    bset PORTB, #13
-    done_with_RS_LCD_8:
-    bset PORTB, #14	; Set E bit
-    /* Another Sketchy Code */
-    MOV PORTB, W3
-    MOV #0xF0FF, W4
-    AND W4, W3, W3	; Preserve value of PORTB except for PORTB[11:8]
-    MOV #0x00F0, W5	
-    AND W5, W0, W5	; Extract W0[7:4]
-    SL	W5, #4, W5	; Shift W0[7:4] to W[11:8]
-    IOR W3, W5, W3	
-    MOV W3, PORTB	; Send DB[7:4] Bits
-    bclr PORTB, #14	; Reset E Bit
-    
-    bset PORTB, #14
-    MOV PORTB, W3
-    MOV #0xF0FF, W4
-    AND W4, W3, W3	; Preserve value of PORTB except for PORTB[11:8]
-    MOV #0x000F, W5
-    AND W5, W0, W5	; Extract W[3:0]
-    SL	W5, #8, W5	; Shift W0[3:0] TO W0[11:8]
-    IOR W3, W5, W3
-    MOV W3, PORTB	; Send DB[3:0]
-    bclr PORTB, #14
+    call send8ToLCD_data
+    call delay15ms
     return 
     
-/* Delays */
+; Used only in initialization    
+send4ToLCD:
+    SL	    W0, #8, W0
+    MOV	    W0,	PORTB
+    nop
+    BSET    PORTB, #14	    ; Set Enable
+    nop
+    BCLR    PORTB, #13	    ; Clear Enable
+    return
+
+; Send 8-bit Instruction
+send8ToLCD:
+    MOV	    #0x00F0, W7
+    AND	    W7, W0, W7
+    SL	    W7, #4, W7
+    MOV	    #0x000F, W8
+    AND	    W8, W0, W8
+    SL	    W8, #8, W8
+    MOV	    W7, PORTB
+    nop
+    BSET    PORTB, #14	    
+    nop
+    BCLR    PORTB, #14
+    nop
+    MOV	    W8, PORTB
+    nop
+    BSET    PORTB, #14
+    nop
+    BCLR    PORTB, #14
+    nop
+    return
+
+; Send 8-bit Data    
+send8ToLCD_data:
+    MOV	    #0x00F0, W7
+    AND	    W7, W0, W7
+    SL	    W7, #4, W7
+    MOV	    #0x000F, W8
+    AND	    W8, W0, W8
+    SL	    W8, #8, W8
+    BSET    W7, #13
+    MOV	    W7, PORTB
+    nop
+    BSET    PORTB, #14
+    nop
+    BCLR    PORTB, #14
+    nop
+    BSET    W8, #13
+    MOV	    W8, PORTB
+    nop
+    BSET    PORTB, #14
+    nop
+    BCLR    PORTB, #14
+    nop
+    return    
+
+; Function: Set Cursor to 2nd line    
+switch_line:
+    MOV #0x00C0, W0		; 0xC0 - 2nd line
+    call send8ToLCD		; 0x80 - 1st line
+    call delay15ms
+    return
+
+; Function: Clear Display AND Entry Mode Set    
+clear_display:
+    mov #0x0001, W0		
+    call send8ToLCD		
+    call delay15ms
+    
+    mov #0x0006, W0		
+    call send8ToLCD		
+    call delay15ms
+    return    
+    
+; ==============================================================================
+    
+; ********************************** Delays ************************************
 delay15ms:
     mov #0x2710, W0
     mov W0, i
@@ -186,7 +255,6 @@ loop15ms:
     dec i
     bra nz, loop15ms
     return
-
 
 delay4_1ms:
     mov #0xAB0, W0
@@ -205,11 +273,8 @@ loop100us:
     dec i
     bra nz, loop100us
     return
-        
+; ==============================================================================
+    
+end:
 .end
-    
-    
-
-
-
 
---
+++ lcd_working.s
@@ -0,0 +1,259 @@
+.include "p24FJ64GA002.inc"
+.global __reset
+    
+config __CONFIG1, FWDTEN_OFF & JTAGEN_OFF
+;config __CONFIG2, POSCMOD_NONE & OSCIOFNC_OFF & FCKSM_CSDCMD & FNOSC_FRCPLL & PLL96MHZ_OFF & PLLDIV_NODIV
+config __CONFIG2, POSCMOD_NONE & OSCIOFNC_OFF & FCKSM_CSDCMD & FNOSC_FRCPLL ;& PLL96MHZ_OFF & PLLDIV_NODIV    
+    
+.bss
+    i: .space 2
+    j: .space 2
+
+.text
+__reset:
+    
+    mov #__SP_init, W15
+    mov #__SPLIM_init, W0
+    mov W0, SPLIM
+    mov #0x1, W0
+    mov W0, TRISB
+    mov #0xFFFF, W0
+    mov W0, AD1PCFG
+    
+/* Main */
+CALL	delay15ms
+    
+MOV	#0x0300, W0		; Function Set Command
+CALL	send4ToLCD
+CALL	delay15ms
+    
+CALL	send4ToLCD
+CALL	delay15ms
+    
+CALL	send4ToLCD
+CALL	delay15ms
+    
+MOV	#0x0200, W0		; Sets interface to 4-bit
+CALL	send4ToLCD
+CALL	delay15ms
+    
+MOV	#0x0200, W0		; F - 0  || N - 1 (2 lines)
+MOV	#0x0800, W1		
+CALL	send8ToLCD  
+CALL	delay15ms
+
+MOV	#0x0000, W0		; Display Off
+MOV	#0x0800, W1
+CALL	send8ToLCD 
+CALL	delay15ms
+ 
+MOV	#0x0000, W0		; Clear Display
+MOV	#0x0100, W1
+CALL	send8ToLCD 
+CALL	delay15ms
+    
+mov #0x0000, W0			; Entry Mode 
+mov #0x0600, W1
+call send8ToLCD 
+call delay15ms
+    
+mov #0x0000, W0			; Display ON
+mov #0x0F00, W1
+call send8ToLCD 
+call delay15ms
+
+    
+    
+/* Main Loop */    
+fl:
+    call clear_display
+    call first_name
+    call switch_line
+    call last_name
+    poll1:
+	mov PORTB, W7  
+	btsc W7, #0	    ; skip next if PORTB<0> is 0
+	goto poll1
+	goto lf
+	
+lf:
+    call clear_display
+    call last_name
+    call switch_line
+    call first_name
+    poll2:
+	mov PORTB, W7
+	btss W7, #0	    ; skip next if PORTB<0> is 1
+	goto poll2
+	goto fl
+    
+    
+first_name:  
+    mov #0x2500, W0	    ; R
+    mov #0x2200, W1         
+    call send8ToLCD 
+    call delay15ms
+    
+    mov #0x2700, W0	    ; u
+    mov #0x2500, W1
+    call send8ToLCD 
+    call delay15ms
+    
+    mov #0x2700, W0	    ; s
+    mov #0x2300, W1
+    call send8ToLCD	    
+    call delay15ms
+    
+    mov #0x2700, W0	    ; t
+    mov #0x2400, W1
+    call send8ToLCD 
+    call delay15ms
+
+    mov #0x2700, W0	    ; y
+    mov #0x2900, W1
+    call send8ToLCD 
+    call delay15ms
+    
+    mov #0x2A00, W0	    ; space
+    mov #0x2000, W1
+    call send8ToLCD	    
+    call delay15ms
+
+    mov #0x2400, W0	    ; J
+    mov #0x2A00, W1         
+    call send8ToLCD 
+    call delay15ms
+    
+    mov #0x2600, W0	    ; o
+    mov #0x2F00, W1
+    call send8ToLCD 
+    call delay15ms
+    
+    mov #0x2600, W0	    ; h
+    mov #0x2800, W1
+    call send8ToLCD	    
+    call delay15ms
+    
+    mov #0x2600, W0	    ; n
+    mov #0x2E00, W1
+    call send8ToLCD 
+    call delay15ms
+
+    mov #0x2A00, W0	    ; space
+    mov #0x2000, W1
+    call send8ToLCD	    
+    call delay15ms    
+    
+    mov #0x2400, W0	    ; L
+    mov #0x2C00, W1
+    call send8ToLCD 
+    call delay15ms
+    
+    mov #0x2600, W0	    ; l
+    mov #0x2c00, W1
+    call send8ToLCD 
+    call delay15ms
+    
+    mov #0x2600, W0	    ; o
+    mov #0x2F00, W1
+    call send8ToLCD 
+    call delay15ms
+    
+    mov #0x2700, W0	    ; y
+    mov #0x2900, W1
+    call send8ToLCD 
+    call delay15ms
+    
+    mov #0x2600, W0	    ; d
+    mov #0x2400, W1
+    call send8ToLCD 
+    call delay15ms
+    return
+    
+last_name:         
+    mov #0x2400, W0	    ; M
+    mov #0x2D00, W1
+    call send8ToLCD	    
+    call delay15ms
+    
+    mov #0x2600, W0	    ; i
+    mov #0x2900, W1
+    call send8ToLCD	    
+    call delay15ms
+    
+    mov #0x2600, W0	    ; n
+    mov #0x2E00, W1
+    call send8ToLCD	    
+    call delay15ms
+
+    mov #0x2600, W0	    ; a
+    mov #0x2100, W1
+    call send8ToLCD	    
+    call delay15ms
+    return 
+        
+/* Functions */
+send4ToLCD:
+    mov W0,PORTB
+    bset PORTB, #14	    ; Set Enable 
+    bclr PORTB, #14	    ; Clear Enable
+    return
+    
+send8ToLCD:
+    mov W0, PORTB
+    bset PORTB, #14	    ; Set Enable
+    bclr PORTB, #14	    ; Clear Enable
+    mov W1, PORTB
+    bset PORTB, #14	    ; Set Enable
+    bclr PORTB, #14	    ; Clear Enable
+    return
+    
+/* Delays */
+delay15ms:
+    mov #0x2710, W0
+    mov W0, i
+    
+loop15ms:
+    dec i
+    bra nz, loop15ms
+    return
+
+delay4_1ms:
+    mov #0xAB0, W0
+    mov W0, i
+    
+loop4_1ms:
+    dec i
+    bra nz, loop4_1ms
+    return
+
+delay100us:
+    mov #0x41, W0
+    mov W0, i
+    
+loop100us:
+    dec i
+    bra nz, loop100us
+    return
+
+switch_line:
+    MOV #0x0C00, W0		; 0xC0 vs 0x80
+    MOV #0x0000, W1
+    call send8ToLCD
+    call delay15ms
+    return
+
+clear_display:
+    mov #0x0000, W0		; Clear Display
+    mov #0x0100, W1
+    call send8ToLCD		
+    call delay15ms
+    
+    mov #0x0000, W0		; Entry Mode Set
+    mov #0x0600, W1
+    call send8ToLCD		
+    call delay15ms
+    return
+    
+end:
+.end
